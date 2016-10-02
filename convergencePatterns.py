97b958db83d714bffd70c944128226e00cefa64a
malancas <malancaster0@gmail.com>
2016-10-02 22:07:30 +0000

    Deleted unneeded result files. Fixed bug in findQuartetConvergencePatterns. Deleted unused convergencePatterns functions

--- convergencePatterns.py
+++ convergencePatterns.py
@@ -26,36 +26,15 @@ class convergencePatterns(object):
                 return map(f, perms)
 
 
-        def parametersAreDifferent(self,p1,p2,p3,p4):
-                return  p3 != p1 and p3 != p2 and p4 != p1 and p4 != p2
-
-
-        def getPairDictTotal(self, permList):
-                p1, p2 = permList[0], permList[1]
-                total = 0
-                perms = (list(permutations(range(1,14),2)))
-                for perm in perms:
-                        if self.parametersAreDifferent(p1,p2,perm[0],perm[1]):
-                                total += self.quartetDict[p1][p2][perm[0]][perm[1]]
-                return total
-
         # 156 total permutations
         def writePairResults(self):
                 outFile = os.path.join(self.outputPath + '_PairConvergenceResults.csv')
                 with open(outFile, 'a') as f:
                         writer = csv.writer(f)
                         writer.writerow(self.getHeader(2))
-                        permList = list(permutations(range(1,14),2))
-                        writer.writerow(map(self.getPairDictTotal,permList))
-
-
-        def getTrioDictTotal(self, permList):
-                p1, p2, p3 = permList[0], permList[1], permList[2]
-                total = 0
-                for i in range(1,14):
-                        if i != p1 and i != p2 and i != p3:
-                                total += self.quartetDict[p1][p2][p3][i]
-                return total
+                        perms = list(permutations(range(1,14),2))
+                        k = lambda perm: self.pairDict[perm[0]][perm[1]]
+                        writer.writerow(map(k,perms))
 
 
         '''
@@ -69,10 +48,11 @@ class convergencePatterns(object):
                         writer = csv.writer(f)
                         header = self.getHeader(3)
                         perms = list(permutations(range(1,14),3))
+                        k = lambda perm: self.trioDict[perm[0]][perm[1]][perm[2]]
                         writer.writerow(header[:1024])
-                        writer.writerow(map(self.getTrioDictTotal,perms[:1024]))
+                        writer.writerow(map(k, perms[:1024]))
                         writer.writerow(header[1024:])
-                        writer.writerow(map(self.getTrioDictTotal,perms[1024:]))
+                        writer.writerow(map(k, perms[1024:]))
 
 
         '''
@@ -89,7 +69,7 @@ class convergencePatterns(object):
                         numberOfRows = len(perms) / 1024
                         startIndex = 0
                         endIndex = 1024
-                        k = lambda perms: self.quartetDict[perms[0]][perms[1]][perms[2]][perms[3]]
+                        k = lambda perm: self.quartetDict[perm[0]][perm[1]][perm[2]][perm[3]]
                         for i in range(numberOfRows):
                                 writer.writerow(header[startIndex:endIndex])
                                 writer.writerow(map(k,perms[startIndex:endIndex]))
@@ -108,62 +88,23 @@ class convergencePatterns(object):
 		print 'Quartet results written to file'
 
 
-	def printConvergencePairs(self):
-		for m in range(0, 13):
-			for n in range(0, 13):
-				if (m != n):
-					try:
-						print "P({0} < {1})".format(m, n)
-						print self.pairDict[m][n]
-						print '\n'
-					except KeyError:
-						pass
-
-
 	# Will check for different sentence convergence foursome
 	# combinations exist. Different combinations are added to quarterDict
 	# if they don't appear, otherwise the count within their corresponding
 	# dictionary entry is incremented
 	def findQuartetConvergencePatterns(self, sortedTCV):
+                countTrio = False
+                countPair = False
 		for i in range(0, 13):
 			for j in range(i+1, 13):
+                                firstParameter = sortedTCV[i][1]
+                                secondParameter = sortedTCV[j][1]
+                                self.pairDict[firstParameter][secondParameter] += 1
 				for k in range(j+1, 13):
+                                        thirdParameter = sortedTCV[k][1]
+                                        self.trioDict[firstParameter][secondParameter][thirdParameter] += 1
 					for l in range(k+1, 13):
-						firstParm = sortedTCV[i][1]
-						secondParm = sortedTCV[j][1]
-						thirdParm = sortedTCV[k][1]
-						fourthParm = sortedTCV[l][1]
-
-						self.quartetDict[firstParm][secondParm][thirdParm][fourthParm] += 1
-                                                self.trioDict[firstParm][secondParm][thirdParm] += 1
-                                                self.pairDict[firstParm][secondParm] += 1
-
-
-	# Will check for different sentence convergence threesome
-	# combinations exist. Different combinations are added to trioDict
-	# if they don't appear, otherwise the count within their corresponding
-	# dictionary entry is incremented
-	def findTrioConvergencePatterns(self, sortedTCV):
-		for i in range(0, 13):
-			for j in range(i+1, 13):
-				for k in range(j+1, 13):
-					firstParm = sortedTCV[i][1]
-					secondParm = sortedTCV[j][1]
-					thirdParm = sortedTCV[k][1]
-					self.trioDict[firstParm][secondParm][thirdParm] += 1
-
-
-	# Checks if key value pairs exist for each possible pairing in the current
-	# sorted time course vector. If not, a pair is made and a count recording
-	# the number of times the ordering appears in every learner. If the pair
-	# already exists, the count is incremented
-	def findConvergencePairs(self, sortedTCV):
-		for i in range(0, 12):
-			for j in range(i+1, 13):
-				firstParm = sortedTCV[i][1]
-				secondParm = sortedTCV[j][1]
-
-				self.pairDict[firstParm][secondParm] += 1
+						self.quartetDict[firstParameter][secondParameter][thirdParameter][sortedTCV[l][1]] += 1
 
 
 	# Will track the different convergence patterns (the order in which each parameter converges)
@@ -174,7 +115,5 @@ class convergencePatterns(object):
 			# Sort the learner's timeCourseVector based on the convergence
 			# time of each parameter
 			sortedTCV = sorted(tcv, key=lambda parameter: parameter[0])
-			assert(len(sortedTCV) == 13)
-
 			self.findQuartetConvergencePatterns(sortedTCV)
                 self.writeResults()
