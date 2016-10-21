73dad1b8dbc8273fb60139f0380593c6a4ce1136
malancas <malancaster0@gmail.com>
2016-10-21 14:30:41 +0000

    Updated script to handle indenticle result files already present. Added sub directory to results just for trigger results

--- parameterSentenceTriggers.py
+++ parameterSentenceTriggers.py
@@ -1,10 +1,31 @@
 #!/opt/anaconda/bin/python
 import sys
 import csv
+import os
 from collections import defaultdict
 from Child import Child
 
 targetGrammar = sys.argv[1]
+languageName = ''
+if targetGrammar == '611':
+    languageName = 'English'
+elif targetGrammar == '584':
+    languageName = 'French'
+elif targetGrammar == '2253':
+    languageName = 'German'
+elif targetGrammar == '3856':
+    languageName = 'Japanese'
+
+tempFileName = languageName + '_sentenceParameterTrigger_results.csv'
+tempPathName = './results/sentenceParameterTriggers'
+if not os.path.isdir(tempPathName):
+    os.makedirs(tempPathName)
+outputFile = os.path.join(tempPathName, tempFileName)
+if os.path.isfile(outputFile):
+    answer = input('File already exists. Overwrite? Y/N: ')
+    if answer == 'N' or answer == 'n':
+        sys.exit(0)
+
 infoFile = open('EngFrJapGerm.txt', 'rU')
 allSentences = infoFile.readlines()
 infoFile.close()
@@ -25,18 +46,16 @@ while not foundAllTargetSentences:
 sentenceParameterTriggers = defaultdict(lambda: [])
 sampleLearner = Child()
 oldGrammar = [0,0,0,0,0,0,1,0,0,0,1,0,1]
-print(len(selectedSentences))
 for sentence in selectedSentences:
     sentenceStr = sentence.rsplit('\t', 3)[2]
     sampleLearner.consumeSentence(sentence)
     sampleLearner.setParameters()
-    for i in range(13):
-        if oldGrammar[i] != sampleLearner.grammar[i]:
-            print('Doesnt match')
+    for i, parameter in enumerate(oldGrammar):
+        if parameter != sampleLearner.grammar[i]:
             sentenceParameterTriggers[sentenceStr].append('p{}'.format(i+1))
     oldGrammar = sampleLearner.grammar
 
-with open('sentenceParameterTrigger_results.csv', 'a+') as outFile:
+with open(outputFile, 'a+') as outFile:
     writer = csv.writer(outFile)
     for key in sentenceParameterTriggers:
         writer.writerow((key, sentenceParameterTriggers[key]))
