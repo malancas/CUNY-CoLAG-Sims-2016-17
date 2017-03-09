8ea692b70707f4a6f8e5107f3b19a9c2709d9c7a
malancas <malancaster0@gmail.com>
2017-03-09 18:22:11 -0500

    Added ModelFunctions class file. Created updateWeights and makeGrammar functions

--- Yang-weighted-model/MainModel.scala
+++ Yang-weighted-model/MainModel.scala
@@ -1,4 +1,6 @@
 import scala.io.Source
+import scala.util.Random
+import modelfunctions.ModelFunctions
 
 object MainModel {
   val usage = """
@@ -10,9 +12,17 @@ object MainModel {
       return
     }
 
-    println("Hello, world!")
     // Read target language sentences from EngFrJapGerm.txt
     val targetSentences = io.Source.fromFile("../EngFrJapGerm.txt").getLines.map(x => x.split('\t')).filter(line =>line(0) == args(1)).toArray
-    println(targetSentences.deep.mkString("\n"))
+    //println(targetSentences.deep.mkString("\n"))
+
+    // Set up initial weights
+    val weights = List.fill(13)(0.5)
+
+    val grammar = new ModelFunctions makeGrammar(weights)
+    println(grammar)
+
+    val newWeights = new ModelFunctions updateWeights(weights, grammar, true, List())
+    println(newWeights)
   }
 }
