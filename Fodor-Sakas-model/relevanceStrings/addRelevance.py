bf8e965c4e81d13e285b4eba849aaf7e40729c1b
malancas <malancaster0@gmail.com>
2017-04-18 20:53:40 -0400

    Fixed format error when writing results to a new file

--- Fodor-Sakas-model/relevanceStrings/addRelevance.py
+++ Fodor-Sakas-model/relevanceStrings/addRelevance.py
@@ -8,8 +8,6 @@ with open('COLAG_2011_ids.txt', 'r') as infile:
     for line in content:
         grammId, sentId, structId = line.split('\t')
         bitstring = format(int(grammId), '16b')
-        # print(bitstring)
-        # print(len(bitstring))
 
         if n == -1:
             n = len(bitstring)
@@ -22,15 +20,14 @@ with open('COLAG_2011_ids.txt', 'r') as infile:
 
 relevanceDict = {}
 for key, value in idDict.items():
-    #relevanceDict[key] = getRelevanceString(value, n)
-    relevanceDict.update({key: getRelevanceString(value, n)})
+    relevanceDict[key] = getRelevanceString(value, n)
 
 newLines = []
 
 with open('COLAG_2011_ids.txt', 'r') as f:
     for line in f.readlines():
         grammId, sentId, structId = line.split('\t')
-        newLines.append(' '.join([line, relevanceDict[structId]]))
+        newLines.append(' '.join([line.rstrip('\n'), '\t', relevanceDict[structId], '\n']))
 
 with open('COLAG_2011_ids_relevance.txt', 'w') as f:
     f.writelines(newLines) 
